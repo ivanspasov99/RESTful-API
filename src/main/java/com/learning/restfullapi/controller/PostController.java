81342161565b833d637257093ab887e41ffac844
Spasov <i.spasov@sap.com>
2020-09-17 15:18:08 +0300

    Integration Test Executable Script

--- src/main/java/com/learning/restfullapi/controller/PostController.java
+++ src/main/java/com/learning/restfullapi/controller/PostController.java
@@ -5,7 +5,6 @@ import com.learning.restfullapi.model.Post;
 import com.learning.restfullapi.service.PostsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.PersistenceException;
